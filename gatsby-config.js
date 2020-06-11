7f7488b173ab34ca33064ead893f3d5f54c495f3
Yang Sun <ysun9596@gmail.com>
2020-06-11 10:55:26 -0700

    changed to preact

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     "gatsby-plugin-dark-mode",
+    `gatsby-plugin-preact`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
